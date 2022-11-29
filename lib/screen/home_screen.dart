ec45077041547e7db7fc2390c9b3008c9a86453d
eunji <lisztk31@gmail.com>
2022-11-29 13:34:09 +0900

    SelectProvider 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:lab_inflearn_2/screen/auto_dispose_modifier_screen.dart';
 import 'package:lab_inflearn_2/screen/family_modifier_screen.dart';
 import 'package:lab_inflearn_2/screen/future_provider_screen.dart';
 import 'package:lab_inflearn_2/screen/listen_provider_screen.dart';
+import 'package:lab_inflearn_2/screen/select_provider_screen.dart';
 import 'package:lab_inflearn_2/screen/state_notifier_provider_screen.dart';
 import 'package:lab_inflearn_2/screen/state_provider_screen.dart';
 import 'package:lab_inflearn_2/screen/stream_provider_screen.dart';
@@ -65,6 +66,13 @@ class HomeScreen extends StatelessWidget {
                     MaterialPageRoute(builder: (_) => ListenProviderScreen()));
               },
               child: Text('ListenProviderScreen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => SelectProviderScreen()));
+              },
+              child: Text('SelectProviderScreen'),
             ),
           ],
         ));
