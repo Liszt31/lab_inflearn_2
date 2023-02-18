c018eec01b68dd6a8291ea30b7f8d290b7cb4a64
eunji <lisztk31@gmail.com>
2023-02-18 15:39:29 +0900

    provider observer 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lab_inflearn_2/riverpod/provider_obsever.dart';
 import 'package:lab_inflearn_2/screen/home_screen.dart';
 
 void main() {
   runApp(ProviderScope(
+    observers: [
+      Logger(),
+    ],
     child: MaterialApp(
       home: HomeScreen(),
     ),
